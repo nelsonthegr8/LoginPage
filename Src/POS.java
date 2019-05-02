4a5e4d7e43be75a14c1dc39b2131b991bc5be895
nelsonthegr8 <32414327+nelsonthegr8@users.noreply.github.com>
2019-05-02 17:55:48 -0400

    java
    new Update

--- Src/POS.java
+++ Src/POS.java
@@ -1,18 +1,25 @@
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.text.DecimalFormat;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import javax.swing.JTable;
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 
 public class POS extends JPanel {
-	private JTextField textField;
-	private JTable table;
-
+	private static JTextField textField;
+	private static JTable table = new JTable(new DefaultTableModel(null, new Object[]{"ItemNum","Item", "Price"}));
+	private static String item = "";
+	private boolean check = true;
+	private static JLabel lblTotal = new JLabel("subtotal");
+	private static JLabel lblTotal_1 = new JLabel("total");
 	/**
 	 * Create the panel.
 	 */
@@ -39,29 +46,82 @@ public class POS extends JPanel {
 		textField.setColumns(10);
 		
 		JButton btnAdd = new JButton("Add");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					item = textField.getText();
+				}catch(NullPointerException f) {
+					check = false;
+				}
+				
+			if(check) {
+				try {
+					functions.refreshTable(item);
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			check = true;
+		}	
+		});
 		btnAdd.setBounds(508, 13, 97, 25);
 		add(btnAdd);
 		
 		JScrollPane scrollPane = new JScrollPane(table);
-		scrollPane.setBounds(0, 0, 2, 2);
+		scrollPane.setBounds(26, 46, 521, 437);
 		add(scrollPane);
 		
-		table = new JTable();
-		table.setBounds(26, 46, 521, 437);
-		add(table);
 		
-		JLabel lblTime = new JLabel("time");
-		lblTime.setBounds(480, 522, 56, 16);
-		add(lblTime);
-		
-		JLabel lblTotal = new JLabel("total");
-		lblTotal.setHorizontalAlignment(SwingConstants.CENTER);
-		lblTotal.setBounds(721, 391, 135, 16);
+		lblTotal.setHorizontalAlignment(SwingConstants.LEFT);
+		lblTotal.setBounds(744, 391, 135, 16);
 		add(lblTotal);
 		
 		JButton btnFinished = new JButton("Finished");
+		btnFinished.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				double amount = Float.valueOf((String) lblTotal.getText().trim());
+				functions.transactionFinished(amount);
+			}
+		});
 		btnFinished.setBounds(744, 481, 97, 25);
 		add(btnFinished);
+		
+		JLabel lblSubtotal = new JLabel("Subtotal");
+		lblSubtotal.setBounds(658, 392, 53, 14);
+		add(lblSubtotal);
+		lblTotal_1.setHorizontalAlignment(SwingConstants.LEFT);
+		
+		
+		lblTotal_1.setBounds(744, 417, 46, 14);
+		add(lblTotal_1);
+		
+		JLabel lblTotal_2 = new JLabel("Total");
+		lblTotal_2.setBounds(658, 417, 46, 14);
+		add(lblTotal_2);
 
+	}
+	
+	public static void setTable(String type,String price) {
+		DefaultTableModel model = (DefaultTableModel) table.getModel();
+		model.addRow(new Object[]{item,type, price});
+		textField.setText("");
+		lblTotal.setText(functions.totalAmount());
+		double total = Float.valueOf((String) lblTotal.getText().trim());
+		total = total + (total * .06);
+		DecimalFormat df = new DecimalFormat("###.##");
+		lblTotal_1.setText(" "+df.format(total));
+	}
+	
+	public static JTable getTable() {
+		return table;
+	}
+	
+	public static void resetFields() {
+		textField.setText("");
+		lblTotal.setText("");
+		lblTotal_1.setText("");
+		DefaultTableModel model = (DefaultTableModel) table.getModel();
+		model.setRowCount(0);
 	}
 }
--- Src/functions.java
+++ Src/functions.java
@@ -1,21 +1,31 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Set;
 
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.JTable;
+
+import net.proteanit.sql.DbUtils;
 
 public class functions {
 //initialize the variables that will be passed around the application
 public static String CurrentusrName = "";
 private static JPanel Login = new LogIn();
-private static JPanel PoS = new POS();
+private static JPanel PiS = new POS();
 private static JPanel panelholder = new panelHolder();
-
+private static double amount = 0.0;
 //this function is called when the user tries to log in and it is successful or not
 	public static void loginQuery(String uname, String pass) throws SQLException {
 		// 1. Establish a Connection
@@ -29,22 +39,169 @@ private static JPanel panelholder = new panelHolder();
 		ResultSet myRs = myStmt.executeQuery();
 		if(myRs.next() == false) {
 			JOptionPane.showMessageDialog( null , "Username or Password is Incorrect");
+			myConn.close();
 		}else {
 			JOptionPane.showMessageDialog( null , "Login Successful");
 			CurrentusrName = uname;
 			Main_Window.changePanel(functions.panelholder);
 			LogIn.resetFields();
+			myConn.close();
 		}
 	}
 	
-	public static ArrayList<Merchandise> MerchandiseList(){
-		ArrayList<Merchandise> merch = new ArrayList<>();
-		
-		return merch;
+//this functions sets the POS Table	
+	public static void refreshTable(String item) throws SQLException {
+		// 1. Establish a Connection
+		Connection myConn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Users;integratedSecurity=true");
+		// 2. Prepare Statement
+		PreparedStatement myStmt = myConn.prepareStatement("select * from Merchandise where Item_Number = ?");
+		//
+		myStmt.setString(1, item);
+		//4. execute sql query
+		ResultSet myRs = myStmt.executeQuery();
+		if(myRs.next() == false) {
+			myConn.close();
+		}else {
+			
+			POS.setTable(myRs.getString("Type"),myRs.getString("Price"));
+			myConn.close();
+		}
 	}
 	
-	public static void RefreshTable() {
-		
+	//this functions sets the POS Table	
+		public static int Quantities(String item) throws SQLException {
+			int q = 0;
+			// 1. Establish a Connection
+			Connection myConn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Users;integratedSecurity=true");
+			// 2. Prepare Statement
+			PreparedStatement myStmt = myConn.prepareStatement("select Quantity from Merchandise where Item_Number = ?");
+			//
+			myStmt.setString(1, item);
+			//4. execute sql query
+			ResultSet myRs = myStmt.executeQuery();
+			if(myRs.next() == false) {
+				myConn.close();
+			}else {
+				q = myRs.getInt("Quantity");
+				myConn.close();
+			}
+			return q;
+		}
+//this function gives a current total of the purchase
+	public static String totalAmount() {
+		amount = 0.0;
+		DecimalFormat df = new DecimalFormat("###.##");
+		JTable table = POS.getTable();
+		for(int i = 0; i < table.getRowCount(); i++) {
+			amount += Float.valueOf((String) table.getValueAt(i, 2));
+		}
+		return " "+df.format(amount);
+	}
+//this function outputs the information to a txt document and to the sales database and updates the quantities left in stock	
+	public static void transactionFinished(double total) {
+		DecimalFormat df = new DecimalFormat("###.##");
+		String reciept = "receipt.txt";
+		SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+		Date date = new Date();
+		HashMap<String, Integer> map = new HashMap<>();
+		try {
+			//this part outputs the info to a receipt
+			PrintWriter outputStream = new PrintWriter(reciept);
+			outputStream.println("\t Receipt\n");
+			outputStream.println("Date: "+formatter.format(date)+"\n");
+			JTable table = POS.getTable();
+			for(int i = 0; i < table.getRowCount(); i++) {
+				outputStream.println("Item: "+table.getValueAt(i, 1)+"\t Price: $"+table.getValueAt(i, 2));
+				if(map.containsKey(table.getValueAt(i, 0))){
+		            map.put((String) table.getValueAt(i, 0), map.get(table.getValueAt(i, 0)) + 1); 
+		        }else {
+		        	map.put((String) table.getValueAt(i, 0), 1);
+		        }
+			}
+			//this gets the info from the hash map and stores it into a temporary set so i can pass the variables throgh a query and update the Inventory table inside of the database
+			String[] items = new String[map.keySet().size()];
+			int[] Quantities = new int[map.values().size()];
+			int i = 0;
+			int j = 0;
+			for(int n : map.values()) {
+				Quantities[i] = n;
+				i++;
+			}
+			for(String n : map.keySet()) {
+				items[j] = n;
+				j++;
+			}
+			outputStream.println("\nSubTotal: "+df.format(total));
+			outputStream.println("Total: "+df.format((total * .06) + total));
+			outputStream.close();
+			//this part updates the sql server inventory by calling a function that queries to the database
+			for(int count = 0; count < items.length; count++) {
+				try {
+					//this calls the functions to update the merchandise database
+					updateInventory((Quantities(items[count]) - Quantities[count]),items[count]);
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					//e.printStackTrace();
+				}
+			}
+			//calls function that adds to sales table
+			
+			//this part resets the fields of the pos register
+			POS.resetFields();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void updateInventory(int quant, String item) throws SQLException {
+		// 1. Establish a Connection
+		Connection myConn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Users;integratedSecurity=true");
+		// 2. Prepare Statement
+		PreparedStatement myStmt = myConn.prepareStatement("UPDATE Merchandise\r\n" + 
+				"SET Quantity = ?\r\n" + 
+				"WHERE Item_Number =?;");
+		//
+		myStmt.setInt(1, quant);
+		myStmt.setString(2, item);
+		//4. execute sql query
+		ResultSet myRs = myStmt.executeQuery();
+		if(myRs.next() == false) {
+			myConn.close();
+		}else {
+			//POS.setTable(DbUtils.resultSetToTableModel(myRs));
+			myConn.close();
+		}
+	}
+	
+	public static void refreshSalesTable() throws SQLException {
+		// 1. Establish a Connection
+		Connection myConn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Users;integratedSecurity=true");
+		// 2. Prepare Statement
+		PreparedStatement myStmt = myConn.prepareStatement("select * from Sales");
+		//4. execute sql query
+		ResultSet myRs = myStmt.executeQuery();
+		if(myRs.next() == false) {
+			myConn.close();
+		}else {
+			//POS.setTable(DbUtils.resultSetToTableModel(myRs));
+			myConn.close();
+		}
+	}
+	
+	public static void refreshMerchTable(String item) throws SQLException {
+		// 1. Establish a Connection
+		Connection myConn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databaseName=Users;integratedSecurity=true");
+		// 2. Prepare Statement
+		PreparedStatement myStmt = myConn.prepareStatement("select * from Merchandise");
+		//4. execute sql query
+		ResultSet myRs = myStmt.executeQuery();
+		if(myRs.next() == false) {
+			myConn.close();
+		}else {
+			//POS.setTable(DbUtils.resultSetToTableModel(myRs));
+			myConn.close();
+		}
 	}
 	
 	public static JPanel login() {
@@ -56,6 +213,6 @@ private static JPanel panelholder = new panelHolder();
 	}
 	
 	public static JPanel POS() {
-		return PoS;
+		return PiS;
 	}
 }
